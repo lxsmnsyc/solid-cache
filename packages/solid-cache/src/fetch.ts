688c5febf269a8869e1ba56ed0629d7b58236fb0
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-03 17:03:39 +0800

    Add `fetch`

---
+++ packages/solid-cache/src/fetch.ts
@@ -0,0 +1,103 @@
+import { CachedResource, createCachedResource } from './core';
+
+const nativeFetch = globalThis.fetch;
+
+type Signalify<T> = T | (() => T);
+
+function isSignal<T>(value: Signalify<T>): value is () => T {
+  return typeof value === 'function';
+}
+
+function fromSignal<T>(value: Signalify<T>): T {
+  if (isSignal(value)) {
+    return value();
+  }
+  return value;
+}
+
+function serializeInput(input: RequestInfo | URL) {
+  if (typeof input === 'string') {
+    return input;
+  }
+  if (input instanceof URL) {
+    return input.toString();
+  }
+  return input.url;
+}
+
+export class FetchResponse {
+  private input: Signalify<RequestInfo | URL>;
+
+  private init?: Signalify<RequestInit | undefined>;
+
+  constructor(
+    input: Signalify<RequestInfo | URL>,
+    init?: Signalify<RequestInit | undefined>,
+  ) {
+    this.input = input;
+    this.init = init;
+  }
+
+  arrayBuffer(): CachedResource<ArrayBuffer | undefined> {
+    return createCachedResource(
+      () => fromSignal(this.input),
+      serializeInput,
+      async (localInput) => {
+        const response = await nativeFetch(localInput, fromSignal(this.init));
+        return response.arrayBuffer();
+      },
+    );
+  }
+
+  blob(): CachedResource<Blob | undefined> {
+    return createCachedResource(
+      () => fromSignal(this.input),
+      serializeInput,
+      async (localInput) => {
+        const response = await nativeFetch(localInput, fromSignal(this.init));
+        return response.blob();
+      },
+    );
+  }
+
+  formData(): CachedResource<FormData | undefined> {
+    return createCachedResource(
+      () => fromSignal(this.input),
+      serializeInput,
+      async (localInput) => {
+        const response = await nativeFetch(localInput, fromSignal(this.init));
+        return response.formData();
+      },
+    );
+  }
+
+  json<T>(): CachedResource<T> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return createCachedResource(
+      () => fromSignal(this.input),
+      serializeInput,
+      async (localInput) => {
+        const response = await nativeFetch(localInput, fromSignal(this.init));
+        return response.json();
+      },
+    );
+  }
+
+  text(): CachedResource<string | undefined> {
+    return createCachedResource(
+      () => fromSignal(this.input),
+      serializeInput,
+      async (localInput) => {
+        const response = await nativeFetch(localInput, fromSignal(this.init));
+        return response.text();
+      },
+    );
+  }
+}
+
+export function fetch(
+  input: Signalify<RequestInfo | URL>,
+  init?: Signalify<RequestInit | undefined>,
+): FetchResponse {
+  return new FetchResponse(input, init);
+}
